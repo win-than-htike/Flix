25fed5f8e983151e5312c8a62734e9f8fe3fcc70
Win Than Htike <winthan.htike@7peakssoftware.com>
2024-12-26 00:57:29 +0700

    Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_25_171024) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_25_175014) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 end
