53ea064885747c665ba0a4562739206d9ce48466
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-02 14:14:58 +0700

    The Flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully updated!"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully created!"
     else
       render :new, status: :unprocessable_entity
     end
@@ -36,7 +36,8 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_path, status: :see_other
+    redirect_to movies_path, status: :see_other,
+      danger: "Movie successfully delete!"
   end
 
 private
