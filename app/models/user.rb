0f6d6465bdb294d72347a61f7f6cc6b24807e95f
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-05 17:58:36 +0700

    Edit User Account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username, presence: true,
+                     format: { with: /\A[A-Z0-9]+\z/i },
+                     uniqueness: { case_sensitive: false }
+
   validates :name, presence: true
 
   validates :email, presence: true,
