f2d12c03bd383ddaa96e9cbdfda89e35f46fcae4
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-13 23:04:37 +0700

    Sign Out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to movies_path, status: :see_other, alret: "Account successfully deleted!"
   end
 
