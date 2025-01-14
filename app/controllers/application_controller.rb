baf8f1d21fcf73f53f70fc30806c45726f3d7297
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-14 21:24:48 +0700

    Authorization Part-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   add_flash_types(:danger)
+
+private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    unless current_user
+      redirect_to new_session_path, alert: "Please sign in first!"
+    end
+  end
+
 end
