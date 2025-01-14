a03de4e3a336af53031dc72116a9288ebaacc5ad
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-15 00:15:22 +0700

    Authorization Part-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,15 @@ private
 
   helper_method :current_user
 
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user?
+
   def require_signin
     unless current_user
+      session[:intended_url] = request.url
       redirect_to new_session_path, alert: "Please sign in first!"
     end
   end
