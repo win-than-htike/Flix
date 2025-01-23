1d30a96bd94620effacf0bca43e63b3c62482ce3
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-23 16:49:52 +0700

    Add Genres CRUD

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,52 @@
 class GenresController < ApplicationController
+  before_action :require_signin, except: [:index, :show]
+  before_action :require_admin, except: [:index, :show]
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_path, notice: "Genre successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(movie_params)
+      redirect_to genres_path, notice: "Genre successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to movies_path, status: :see_other,
+      danger: "Genre successfully delete!"
+  end
+
+private
+
+  def genre_params
+    params.require(:genre).
+      permit(:name)
+  end
 end
